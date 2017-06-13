ce47caac2d331f26af7793470996e084b2daee01
yjj <sampson3625@163.com>
2017-06-14 02:39:24 +0800

    litepal配置，省市县实体类

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,10 @@
+package com.coolweather.android.util;
+
+/**
+ * Created by yjj on 2017/6/2.
+ */
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttp
+    }
+}
